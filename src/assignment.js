ce2eeaabd82ab61bd76ce4615b357ce16f708048
crosshaired <thecrosshaired@gmail.com>
2022-07-16 02:45:50 +0100

    Update assignment.js

--- src/assignment.js
+++ src/assignment.js
@@ -34,7 +34,7 @@ assignment.sumOfNumbers = sumOfNumbers;
 function countEvenNumbers(arrayOfNumbers) {
     for (let i = 0; i < arrayOfNumbers.length; i++){
         if(arrayOfNumbers[i] % 2 == 0) {
-            return(arrayOfNumbers[i])
+            return count;
         }
     }
 
